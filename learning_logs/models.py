3586084a4486eee2fd9e6092ef22f9415878fd83
Zack Snider <zacksnider@Zacks-MacBook-Pro.local>
2020-05-11 14:16:17 -0500

    define entry model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,6 +1,8 @@
+
 from django.db import models
 
 # Create your models here.
+
 
 class Topic(models.Model):
     text = models.CharField(max_length=200)
@@ -9,4 +11,19 @@ class Topic(models.Model):
     
     def __str__(self):
         return self.text
+    
+
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        #it allows us to set a special attribute telling Django to use 'Entries'
+        # when it needs  to refer to more than one entry. Without this, Django
+        # would refer to multiple entries as 'Entrys'.
+        verbose_name_plural = 'entries'
+        
+    def __str__(self):
+        return f"{self.text[:50]}..."
     
