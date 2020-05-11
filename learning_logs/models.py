2023d52120b022e85810cbdaf3576b3592af6386
Zack Snider <zacksnider@Zacks-MacBook-Pro.local>
2020-05-11 13:46:14 -0500

    define the model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    text = models.CharField(max_length=200)
+    #auto_now_add=True - set this attribute to the current date and time
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.text
+    
