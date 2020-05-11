066daa3d826e2958c1d609b1e382a4b29820114d
Zack Snider <zacksnider@Zacks-MacBook-Pro.local>
2020-05-11 16:26:29 -0500

    added OG base.html and child template index.html

---
+++ learning_logs/urls.py
@@ -0,0 +1,21 @@
+
+#the path function, which is needed when mapping URLs to views
+from django.urls import path
+
+#the dot (unix) tells Python to import the views.py module from
+# the same directory as the current urls.py module.
+from . import views
+
+#The variable app_name helps Django distinguis this urls.py file from
+# files of the same name in other apps within the project
+app_name = 'learning_logs'
+
+#The variable urlpatterns in this module is a list of individual pages
+# that can be requested from the learning_logs app
+urlpatterns = [
+    #The first argument is an empty string ('') which matches the
+    # base URL - http://localhost:8000/. the second argument specifies
+    # the function name to call in views.py. the third argument provides
+    # the name 'index' for this  URL pattern to refer to it later
+    path('', views.index, name='index'),
+]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# When a URL request matches the pattern we just defined,
+# Django looks for a function called index() in the views.py file.
+
+def index(request):
+    """The home page for Learning Log."""
+    return render(request, 'learning_logs/index.html')
