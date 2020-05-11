38b32b2d07ff21cfea6138e8cd63a9bb9b415ad4
Zack Snider <zacksnider@Zacks-MacBook-Pro.local>
2020-05-11 15:47:14 -0500

    built django shell

---
+++ MyShell.py
@@ -0,0 +1,26 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_log.settings")
+
+import django
+django.setup()
+
+from learning_logs.models import Topic
+
+topics = Topic.objects.all()
+
+for topic in topics:
+    print(topic.id, topic)
+    
+#If you know the ID of a particular object, you can use the method Topic.objects.get()
+# to retrieve that object and examnie any attribute the object has.
+
+t = Topic.objects.get(id=1)
+print(t.text)
+print(t.date_added)
+
+#To get data through a foreign key relationship, you use the lowercase name of the 
+# related model followed by an underscore and the word set
+entries = t.entry_set.all()
+
+for entry in entries:
+    print(entry)
